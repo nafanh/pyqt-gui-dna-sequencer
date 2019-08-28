a5dc3555f0629c32470db9d33e256f5279dcaa6f
nafanh <topkekoutof10@gmail.com>
2019-08-28 02:22:58 -0500

    Update skip_frac.py

--- skip_frac.py
+++ skip_frac.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 # -*- coding: utf-8 -*-
 
 # Form implementation generated from reading ui file 'skip_frac.ui'
@@ -60,7 +60,7 @@ if __name__ == "__main__":
     ui.setupUi(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
-=======
+
 # -*- coding: utf-8 -*-
 
 # Form implementation generated from reading ui file 'skip_frac.ui'
@@ -70,56 +70,56 @@ if __name__ == "__main__":
 # WARNING! All changes made in this file will be lost!
 
 
-from PyQt5 import QtCore, QtGui, QtWidgets
-
-
-class Ui_MainWindow(object):
-    def setupUi(self, MainWindow):
-        MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(615, 446)
-        self.centralwidget = QtWidgets.QWidget(MainWindow)
-        self.centralwidget.setObjectName("centralwidget")
-        self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(190, 30, 291, 221))
-        self.label.setObjectName("label")
-        self.layoutWidget = QtWidgets.QWidget(self.centralwidget)
-        self.layoutWidget.setGeometry(QtCore.QRect(210, 280, 195, 30))
-        self.layoutWidget.setObjectName("layoutWidget")
-        self.horizontalLayout = QtWidgets.QHBoxLayout(self.layoutWidget)
-        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
-        self.horizontalLayout.setObjectName("horizontalLayout")
-        self.pushButton = QtWidgets.QPushButton(self.layoutWidget)
-        self.pushButton.setObjectName("pushButton")
-        self.horizontalLayout.addWidget(self.pushButton)
-        self.pushButton_2 = QtWidgets.QPushButton(self.layoutWidget)
-        self.pushButton_2.setObjectName("pushButton_2")
-        self.horizontalLayout.addWidget(self.pushButton_2)
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 615, 26))
-        self.menubar.setObjectName("menubar")
-        MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QtWidgets.QStatusBar(MainWindow)
-        self.statusbar.setObjectName("statusbar")
-        MainWindow.setStatusBar(self.statusbar)
-
-        self.retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
-
-    def retranslateUi(self, MainWindow):
-        _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "Skip Frac vs. Size?"))
-        self.label.setText(_translate("MainWindow", "Do you want to skip Fractional Area vs. Size"))
-        self.pushButton.setText(_translate("MainWindow", "Yes"))
-        self.pushButton_2.setText(_translate("MainWindow", "No"))
-
-
-if __name__ == "__main__":
-    import sys
-    app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
-    sys.exit(app.exec_())
->>>>>>> 14dd7a7d363abd73c3382bddcb4bbae63a207333
+# from PyQt5 import QtCore, QtGui, QtWidgets
+#
+#
+# class Ui_MainWindow(object):
+#     def setupUi(self, MainWindow):
+#         MainWindow.setObjectName("MainWindow")
+#         MainWindow.resize(615, 446)
+#         self.centralwidget = QtWidgets.QWidget(MainWindow)
+#         self.centralwidget.setObjectName("centralwidget")
+#         self.label = QtWidgets.QLabel(self.centralwidget)
+#         self.label.setGeometry(QtCore.QRect(190, 30, 291, 221))
+#         self.label.setObjectName("label")
+#         self.layoutWidget = QtWidgets.QWidget(self.centralwidget)
+#         self.layoutWidget.setGeometry(QtCore.QRect(210, 280, 195, 30))
+#         self.layoutWidget.setObjectName("layoutWidget")
+#         self.horizontalLayout = QtWidgets.QHBoxLayout(self.layoutWidget)
+#         self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+#         self.horizontalLayout.setObjectName("horizontalLayout")
+#         self.pushButton = QtWidgets.QPushButton(self.layoutWidget)
+#         self.pushButton.setObjectName("pushButton")
+#         self.horizontalLayout.addWidget(self.pushButton)
+#         self.pushButton_2 = QtWidgets.QPushButton(self.layoutWidget)
+#         self.pushButton_2.setObjectName("pushButton_2")
+#         self.horizontalLayout.addWidget(self.pushButton_2)
+#         MainWindow.setCentralWidget(self.centralwidget)
+#         self.menubar = QtWidgets.QMenuBar(MainWindow)
+#         self.menubar.setGeometry(QtCore.QRect(0, 0, 615, 26))
+#         self.menubar.setObjectName("menubar")
+#         MainWindow.setMenuBar(self.menubar)
+#         self.statusbar = QtWidgets.QStatusBar(MainWindow)
+#         self.statusbar.setObjectName("statusbar")
+#         MainWindow.setStatusBar(self.statusbar)
+#
+#         self.retranslateUi(MainWindow)
+#         QtCore.QMetaObject.connectSlotsByName(MainWindow)
+#
+#     def retranslateUi(self, MainWindow):
+#         _translate = QtCore.QCoreApplication.translate
+#         MainWindow.setWindowTitle(_translate("MainWindow", "Skip Frac vs. Size?"))
+#         self.label.setText(_translate("MainWindow", "Do you want to skip Fractional Area vs. Size"))
+#         self.pushButton.setText(_translate("MainWindow", "Yes"))
+#         self.pushButton_2.setText(_translate("MainWindow", "No"))
+#
+#
+# if __name__ == "__main__":
+#     import sys
+#     app = QtWidgets.QApplication(sys.argv)
+#     MainWindow = QtWidgets.QMainWindow()
+#     ui = Ui_MainWindow()
+#     ui.setupUi(MainWindow)
+#     MainWindow.show()
+#     sys.exit(app.exec_())
+#
